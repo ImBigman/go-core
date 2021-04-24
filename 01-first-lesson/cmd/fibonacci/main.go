af9a30feaa8f84fc0102e7636354aa38e610de0b
ImBigman <b1gmanever@gmail.com>
2021-04-24 14:38:09 +0300

    01: correction of comments

--- 01-first-lesson/cmd/fibonacci/main.go
+++ 01-first-lesson/cmd/fibonacci/main.go
@@ -8,17 +8,17 @@ import (
 func main() {
 	var d, e = 8, 19
 
-	print(40, d)
-	print(20, e)
-	print(20, -1)
-	print(20, 22)
+	print(d)
+	print(e)
+	print(-1)
+	print(22)
 }
 
 // print - выводит на экран результат или предупреждения
-// b - длина массива, с - порядковый номер
-func print(b int, c int) {
+// с - порядковый номер
+func print(c int) {
 
-	if 0 < c && c <= b {
+	if 0 < c && c <= 20 {
 		fmt.Println(fibo.Num(c))
 	} else {
 		fmt.Println("Недопустимое значение")
