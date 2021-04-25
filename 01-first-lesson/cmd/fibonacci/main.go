f19953b3a02f8addfe9c813c29bdbefc6531cd17
ImBigman <b1gmanever@gmail.com>
2021-04-25 11:08:06 +0300

    01: refactoring

--- 01-first-lesson/cmd/fibonacci/main.go
+++ 01-first-lesson/cmd/fibonacci/main.go
@@ -8,15 +8,15 @@ import (
 func main() {
 	var d, e = 8, 19
 
-	resultFor(d)
-	resultFor(e)
-	resultFor(-1)
-	resultFor(22)
+	printFiboNUm(d)
+	printFiboNUm(e)
+	printFiboNUm(-1)
+	printFiboNUm(22)
 }
 
-// resultFor - выводит на экран результат или предупреждения
+// printFiboNUm - выводит на экран результат или предупреждения
 // с - порядковый номер
-func resultFor(c int) {
+func printFiboNUm(c int) {
 	if 0 < c && c <= 20 {
 		fmt.Println(fibo.Num(c))
 	} else {
