51fe392e2dae54009fe2c5ba4cea124195953217
ImBigman <b1gmanever@gmail.com>
2021-05-06 12:58:18 +0300

    02: delete 3 methods for refactoring

--- 02-second-lesson/cmd/scanner/main.go
+++ 02-second-lesson/cmd/scanner/main.go
@@ -5,68 +5,43 @@ import (
 	"fmt"
 	"go-core/02-second-lesson/pkg/crawler"
 	"go-core/02-second-lesson/pkg/crawler/spider"
-	"os"
-	"strconv"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Ожидайте, идет сканирование целевых ресурсов.")
-	a := search{"http://go.dev", 5}
-	b := search{"http://golang.org", 2}
-	d, _ := a.Scan()
-	e, _ := b.Scan()
-	res := append(d, e...)
-	if len(res) == 0 {
-		fmt.Println("Результат сканирования отстуствует.")
-		os.Exit(1)
-	}
-
-	sFlag := flag.Bool("s", false, "поиск слов в ссылках после флага -s")
+	sFlag := flag.String("s", "", "поиск слов в ссылках после флага -s")
 	flag.Parse()
-	word := flag.Arg(0)
-
-	if *sFlag && word != "" {
-		resultSlice := pagesContains(res, word)
-		fmt.Printf("Искомая строка: %s, совпадений: %d \n", word, len(resultSlice))
-		fmt.Println("Результат поиска:", returnLinks(resultSlice))
+	fmt.Println("Ожидайте, идет сканирование целевых ресурсов.")
+	resources := targets{{"http://go.dev", 5}, {"http://golang.org", 2}}
+	res, _ := resources.Scan()
+	if *sFlag != "" {
+		fmt.Printf("Искомая строка: %s \n", *sFlag)
+		fmt.Println("Результат поиска:")
+		for _, v := range res {
+			if strings.Contains(strings.ToLower(v.Title), *sFlag) || strings.Contains(strings.ToLower(v.URL), *sFlag) {
+				fmt.Println(v.URL)
+			}
+		}
 	} else {
 		fmt.Println("Результат сканирования: \n", res)
 	}
 }
 
-type doc = crawler.Document
-type pages = []doc
-type search struct {
+type pages = []crawler.Document
+type target struct {
 	url   string
 	depth int
 }
 
-// pagesContains возвращает массив страниц, содержащих переданное значение
-func pagesContains(slice pages, value string) pages {
-	var result pages
-	for _, v := range slice {
-		if strings.Contains(concateFields(v), value) {
-			result = append(result, v)
-		}
-	}
-	return result
-}
+type targets []target
 
-func returnLinks(slice pages) []string {
-	var str []string
-	for i, v := range slice {
-		str = append(str, "\n"+strconv.Itoa(i+1)+": "+v.URL)
-	}
-	return str
-}
-
-func concateFields(d doc) string {
-	return strings.Join([]string{strings.ToLower(d.Title), strings.ToLower(d.URL)}[:], " ")
-}
-
-func (s search) Scan() (pages, error) {
+func (s targets) Scan() (pages, error) {
 	service := spider.New()
-	res, err := service.Scan(s.url, s.depth)
-	return res, err
+	var result pages
+	for _, v := range s {
+		res, _ := service.Scan(v.url, v.depth)
+		result = append(result, res...)
+	}
+
+	return result, nil
 }
--- 02-second-lesson/go.mod
+++
@@ -1,5 +0,0 @@
-module go-core/02-second-lesson
-
-go 1.16
-
-require golang.org/x/net v0.0.0-20210428140749-89ef3d95e781
