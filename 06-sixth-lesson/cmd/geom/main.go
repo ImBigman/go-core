01ab5466a51aa26c25e66053938fb9574a2e7102
ImBigman <b1gmanever@gmail.com>
2021-06-06 11:41:19 +0300

    refactor

--- 06-sixth-lesson/cmd/geom/main.go
+++ 06-sixth-lesson/cmd/geom/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"go-core/06-sixth-lesson/pkg/calc"
+	"go-core/06-sixth-lesson/pkg/measuring"
 )
 
 func main() {
-	coord := calc.Coord{X1: 1.23, X2: 3.45, Y1: 6.78, Y2: 9.1}
-	res := calc.Distance(coord)
-	if res == -1 {
+	coord := measuring.Coord{X1: 1.23, X2: 3.45, Y1: 6.78, Y2: 9.1}
+	if coord.X1 < 0 || coord.X2 < 0 || coord.Y1 < 0 || coord.Y2 < 0 {
 		fmt.Println("Координаты не могут быть меньше нуля")
 	}
-	fmt.Printf("Расстояние между точками: %f", res)
+	fmt.Printf("Расстояние между точками: %f", measuring.Distance(coord))
 }
