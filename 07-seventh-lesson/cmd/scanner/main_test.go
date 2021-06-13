9966d95b156dc9aabcf7ea8ebe367ffab1870a7e
ImBigman <b1gmanever@gmail.com>
2021-06-13 14:19:45 +0300

    07: remove empty test

--- 07-seventh-lesson/cmd/scanner/main_test.go
+++ 07-seventh-lesson/cmd/scanner/main_test.go
@@ -84,16 +84,3 @@ func Test_scan(t *testing.T) {
 		})
 	}
 }
-
-func Test_main(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			main()
-		})
-	}
-}
